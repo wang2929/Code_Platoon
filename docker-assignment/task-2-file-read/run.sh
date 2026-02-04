070c4c4d71a7ebdb7ac53add2c82c068763ff016
wang2929 <tifalsuk@gmail.com>
2026-02-04 13:33:44 +0000

    initial commit from container

---
+++ docker-assignment/task-2-file-read/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# variables you may update to target the correct file
+IMAGE_NAME="task2-demo"
+CONTAINER_NAME="task2-container"
+
+# this will rebuild the image within the docker engine
+echo "Building image: $IMAGE_NAME"
+docker build --quiet -t "$IMAGE_NAME" .
+
+# this will run the container and automatically remove it upon completion
+echo "Running container with bind mount..."
+docker run --rm --name "$CONTAINER_NAME" "$IMAGE_NAME"
---
+++ docker-assignment/task-4-script/task_script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "hello line 1"
+echo "hello line 2"
+echo "hello line 3"
