875ac8e54b687147ea5e891925893fce072a0747
wang2929 <tifalsuk@gmail.com>
2026-02-04 14:01:16 +0000

    bottles of beer py done

--- python-js/algo-99-bottles-py/bottles.py
+++ python-js/algo-99-bottles-py/bottles.py
@@ -1,7 +1,14 @@
+#99 bottles of beer on the wall, 99 bottles of beer.
+#Take one down and pass it around, 98 bottles of beer on the wall.
 def bottle_song():
 	# write your code here!
-	pass
+	for i in range(99, 1, -1):
+		print(str(i) + " bottles of beer on the wall, " + str(i) + " bottles of beer.")
+		print("Take one down and pass it around, " + str(i-1) + " bottles of beer on the wall")
+	print("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.")
+	print("1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.")
+	print("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
 
 bottle_song()
+bottle_song()
 
-####kldjsfalkfdajalskdfjdfls
